41ab4750845ead46d25c32cca4ba62c163594468
jeremy wilson <jeremyw9187@gmail.com>
2021-03-02 19:58:11 -0500

    work in progress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,18 @@
 class GamesController < ApplicationController
+
+  def create
+     @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to new_game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :photo, :description)
+  end
 end
