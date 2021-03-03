ea42a8ac9616a39f2b55bcbeaf3d133c2f1364b8
jeremy wilson <jeremyw9187@gmail.com>
2021-03-02 20:58:57 -0500

    create contoller finished for now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,18 @@
 class GamesController < ApplicationController
 
   def create
-     @game = Game.new(game_params)
-
-    if @game.save
-      redirect_to new_game_path(@game)
-    else
-      render :new
-    end
+     @game = Game.new(params[:game])
+     @game.save
+    # if @game.save
+    #   redirect_to new_game_path(@game)
+    # else
+    #   render :new
+    # end
   end
 
   private
 
-  def game_params
-    params.require(:game).permit(:name, :photo, :description)
-  end
+  # def game_params
+  #   params.require(:game).permit(:name, :photo, :description)
+  # end
 end
