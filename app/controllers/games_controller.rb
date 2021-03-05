c85afe4d6fe00b130fd4c38e5d186a127a045e30
jeremy wilson <jeremyw9187@gmail.com>
2021-03-04 21:21:13 -0500

    new method in games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,16 @@
 class GamesController < ApplicationController
 
-   def index
-      @games = Game.all
-   end
+  def index
+    @games = Game.all
+  end
 
-    def show
-      @game = Game.find(params[:id])
-    end
+  def new
+    @game = Game.new
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
 
   def create
      @game = Game.new(game_params)
