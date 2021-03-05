f0a4fbce9d943950900736c03d10ac6cd38cdddd
jeremy wilson <jeremyw9187@gmail.com>
2021-03-04 20:21:53 -0500

    routes for new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :games, only: [:index, :new, :show, :create]
+  resources :games, only: [:index, :new, :show, :create] do
+    resources :rentals, only: [:new, :create]
+  end
   # get  "/users/:id/games/new/", to: "games#new"
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'index', to: 'games#index'
