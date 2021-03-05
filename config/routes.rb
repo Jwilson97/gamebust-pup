1c0d4cb1910f60f28b2aa73f4ef1167c7648bb3b
Tait Haslam <tait.haslam@gmail.com>
2021-03-04 19:45:16 -0500

    completed refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get  "/users/:id/games/new/",      to: "games#new",  as: :new_game
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'index', to: 'games#index'
-  get "games/:id", to: "games#show", as: :game
-  post 'games', to: 'games#create'
+  resources :games, only: [:index, :new, :show, :create]
+  # get  "/users/:id/games/new/", to: "games#new"
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'index', to: 'games#index'
+  # get "games/:id", to: "games#show", as: :game
+  # post 'games', to: 'games#create'
 
 
 
