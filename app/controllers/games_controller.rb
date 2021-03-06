bc7274167d2cb6629085384d9827a1815d7b448d
Tait Haslam <tait.haslam@gmail.com>
2021-03-06 11:15:01 -0500

    fixed create game issue

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,12 +14,14 @@ class GamesController < ApplicationController
 
   def create
      @game = Game.new(game_params)
+     @game.user = current_user
      @game.save
-    if @game.save
-      redirect_to new_game_path(@game)
-    else
+     if @game.save
+      redirect_to game_path(@game)
+     else
+
       render :new
-    end
+     end
   end
 
   private
