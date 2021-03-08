81002e1507ed887f2d2441cec84402c8f9f66be3
Dima Savchenko <savvaq@gmail.com>
2021-03-08 14:30:29 -0500

    work in progress on implementing pundit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,15 +6,19 @@ class GamesController < ApplicationController
 
   def new
     @game = Game.new
+    authorize @game
   end
 
   def show
     @game = Game.find(params[:id])
+    @authorize @game
   end
 
   def create
     @game = Game.new(game_params)
     @game.user = current_user
+    authorize @game
+
     @game.save
     if @game.save
       redirect_to game_path(@game)
